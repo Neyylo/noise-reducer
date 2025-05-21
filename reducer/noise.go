18294218199506d963e412f8083e31305c18cefb
Neyylo <nolan.dugue@gmail.com>
2025-05-21 13:12:08 +0200

    Feature : LowPassFilter

--- reducer/noise.go
+++ reducer/noise.go
@@ -1,5 +1,13 @@
 package noise
 
-func noise() {
+func LowPassFilter(input []float64, alpha float64) []float64 {
+	output := make([]float64, len(input))
+	if len(input) == 0 {
+		return output
+	}
+	for i := 1; i < len(input); i++ {
+		output[i] = alpha*input[i] + (1-alpha)*output[i-1]
+	}
 
+	return output
 }
