acebbe5902a7fbbfd6c10d3861a57757033aeeb8
Nolan <nolan.dugue@gmail.com>
2025-06-24 14:59:27 +0200

    aaaaa

--- demo/main.go
+++ demo/main.go
@@ -7,8 +7,14 @@ import (
 )
 
 func main() {
-	err := reducer.ProcessFFTBandpass("./audio/heart.wav", "./audio/output_filtered.wav", 44100, 40, 250)
+	input := "./audio/heart.wav"
+	output := "./audio/outputFFT.wav"
+
+	// OU filtre fréquentiel (FFT)
+	err := reducer.ProcessFFTLowPass(input, output, 200.0)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("Traitement terminé avec succès !")
 }
